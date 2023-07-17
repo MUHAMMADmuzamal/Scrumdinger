4901edfc8e7e9eec87371fcaa4207ff850d8e544
Muhammad Muzamal <muhammadmuzamal42@gmail.com>
2023-07-17 06:52:03 +0500

    layout done of meeting view page first commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Muhammad muzamal on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
